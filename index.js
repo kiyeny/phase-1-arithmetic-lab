224ec861a1c4b91159456758c435eae780d1d5fc
maxwell <maxwell.kiyeny@student.moringaschool.com>
2022-06-08 10:29:08 +0300

    my second commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code here!
+const num1=2;
+const num2=31
+let multiply =num1*num2;
+let random = Math.floor(Math.random() * 10);
+let num3 = 26;
+let num4 = 11;
+let mod = num3 % num4;
+let max = Math.max(14, 12, 20);
